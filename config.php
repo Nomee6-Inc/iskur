993fc6db37223fcb70a7cbb278bc19c10e0a445f
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-10-30 18:51:02 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,19 @@
 <?php 
 
+$edevletapitoken = "";
+
 $server = "localhost";
 $user = "";
 $pass = "";
 $database = "";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
+
+try {
+     $db = new PDO("mysql:host=localhost;dbname=$database;charset=utf8", "$user", "$pass");
+} catch ( PDOException $e ){
+     print $e->getMessage();
+}
 
 if (!$conn) {
     die("<script>alert('Veritabanına bağlanırken bir hata oluştu.')</script>");
