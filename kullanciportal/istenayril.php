ca66b22b975635fb9159128e44d422ab5c8e5e8c
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-10-30 18:55:52 +0300

    Update istenayril.php

--- kullanciportal/istenayril.php
+++ kullanciportal/istenayril.php
@@ -1,40 +1,45 @@
 <?php
 session_start();
 include '../config.php';
-$getusername = $_SESSION['username'];
-if (!isset($_SESSION['username'])) {
+$get_user_id = $_SESSION['user_id'];
+
+function getnowDate(){
+$gun=date("d");
+$ay=date("m")-1;
+$yil=date("Y");
+$aylar=Array("Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık");
+$get_clock = date("H:i");
+return "$gun $aylar[$ay] $yil, $get_clock";
+}
+$getnow_date = getnowDate();
+
+if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
 } else {
-    $query = mysqli_query($conn, "SELECT * FROM users WHERE username = '$getusername'");
-    $result = $query->fetch_assoc();
-    $work = $result['work'];
-    $jsonitem = file_get_contents("../works.json");
-    $objitems = json_decode($jsonitem);
-    $findcalisansayisi = function($id) use ($objitems) {
-        foreach ($objitems as $clsnsayisifind) {
-            if ($clsnsayisifind->id == $id) return $clsnsayisifind->calisansayisi;
-        }
-        return false;
-    };
-    $getclsnsysi = $findcalisansayisi($work);
-    $datao = file_get_contents('../works.json');
-
-    $json_arro = json_decode($datao, true);
-
-    foreach ($json_arro as $keyo => $valueo) {
-        if ($valueo['id'] == "$work") {
-            $json_arro[$keyo]['calisansayisi'] = $getclsnsysi-1;
-        }
-    }
-    file_put_contents('../works.json', json_encode($json_arro));
-    $sql3 = "UPDATE users SET work = '' WHERE username = '$getusername'";
-    $run_query1 = mysqli_query($conn, $sql3);
-    $sql31 = "UPDATE users SET workgirisdate = '' WHERE username = '$getusername'";
-    $run_query2 = mysqli_query($conn, $sql31);
-    if($run_query1 && $run_query2) {
-        header("Location: https://iskur.nomee6.xyz/kullanciportal");
-    } else {
-        echo "Bir hata oluştu!";
-    }
+	$worker_q = $db->query("SELECT * FROM workers WHERE user_id = '{$get_user_id}' AND status = 'active'",PDO::FETCH_ASSOC);
+	$worker_q_query = $worker_q->fetch(PDO::FETCH_ASSOC);
+	$worker_q_count = $worker_q -> rowCount();
+	
+	$_get_worker_id = $worker_q_query['id'];
+	
+	if($worker_q_count > 0) {
+		$worker_update_q = $db->prepare("UPDATE workers SET
+			status = :status,
+			leave_date = :leavedate
+			WHERE id = :id");
+		$worker_update_q_query = $worker_update_q->execute(array(
+     		"status" => "inactive",
+			"leavedate" => "$getnow_date",
+     		"id" => $_get_worker_id
+		));
+		
+		if($worker_update_q_query) {
+        	header("Location: https://iskur.nomee6.xyz/kullanciportal");
+    	} else {
+    	    echo "Bir hata oluştu!";
+    	}
+	} else {
+		header("Location: https://iskur.nomee6.xyz/kullanciportal");
+	}
 }
 ?>
